6db74c48280ce6aeed789048828edb9552468bc6
markoblagus <markoblaguss@gmail.com>
2025-12-04 13:09:30 +0100

    Breaking the test on purpose

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const { hello } = require('./index');
 
-assert.strictEqual(hello(), 'hello world');
+assert.strictEqual(hello(), 'HELLO world'); // WRONG on purpose
 assert.strictEqual(hello('marko'), 'hello marko');
 
 console.log('All tests passed ✅');
